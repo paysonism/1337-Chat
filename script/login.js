41a044757d9b29ef4ff56ef3bfe2f390157cde40
Payson <paysonholmes@yahoo.com>
2024-04-19 11:35:07 -0700

    yes

--- script/login.js
+++ script/login.js
@@ -40,7 +40,7 @@ signupForm.addEventListener("submit", (e) => {
 
 const register = async (payload) => {
     try {
-        const res = await fetch("http://localhost:1337/user/signup", {
+        const res = await fetch("https://one337-chat-server.onrender.com/user/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -87,7 +87,7 @@ loginForm.addEventListener("submit", (e) => {
 
 const login = async (payload) => {
     try {
-        const res = await fetch("http://localhost:1337/user/login", {
+        const res = await fetch("https://one337-chat-server.onrender.com/user/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
